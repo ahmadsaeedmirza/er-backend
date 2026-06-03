6d13114bf58baaba6bce027edff14396b25a3776
ahmadsaeedmirza <ahmadsaeedas023@gmail.com>
2026-06-03 17:22:22 +0500

    fix: explicitly bind server to 0.0.0.0 for container hosting

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
+const server = app.listen(port, "0.0.0.0", () => {
   console.log(`App running on port ${port}`);
 });
 
